6e8615cb3b09229404458f5c2a4656605a59940f
vaklawa <wloxwur@gmail.com>
2026-07-27 13:48:24 +0200

    delete unused functions

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/* Collapse & expand nav */
 function collapseNavWindow(){
     let element=document.getElementById("topnav");
     if(element.className==="nav-window-content"){
@@ -19,26 +20,6 @@ function expandNavWindow(){
     }
 }
 
-/* toggles a specific window */
-function toggleAuxWindow(num){
-    let element=document.getElementById("window" + num);
-    if(element.className==="aux-window-content"){
-        element.className="window-collapsed";
-    }
-    else{
-        element.className="aux-window-content";
-    }
-}
-function toggleNavWindow(num){
-    let element=document.getElementById("window" + num);
-    if(element.className==="nav-window-content"){
-        element.className="nav-window-collapsed";
-    }
-    else{
-        element.className="nav-window-content";
-    }
-}
-
 /* switches between default font (Times New Roman) and JP Hand */
 function changeFont(){
    let poemText=document.getElementById("poem");
@@ -49,33 +30,6 @@ function changeFont(){
         poemText.className="default";
     }
     paperSoundEffect2();
-}
-
-function hideAllWindows(){
-    for(let i=1;i<6;i++){
-        if(document.getElementById("window"+i)===null){break;}
-        document.getElementById("window" + i).className="window-collapsed";
-    }
-}
-
-function showAllWindows(){
-    let element;
-    for(let i=1;i<6;i++){
-        element=document.getElementById("window"+i);
-        if(element.className==="window-collapsed"){
-            element.className="window";
-        }
-    }
-}
-
-function showSpecificWindow(num){
-    hideAllWindows();
-    if(document.getElementById("topnav").className==="nav-window-content"){collapseNavWindow();}
-    let element=document.getElementById("window"+num);
-    if(element!==null){
-        element.className="window";
-        console.log("mostrando ventana "+num);
-    }
 }
 
 // play sound effect when opening a poem
